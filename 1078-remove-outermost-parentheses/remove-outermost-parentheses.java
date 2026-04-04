48f8705b1ff3cde937beb310a660aaab5b21ebbe
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-04-04 20:25:02 +0530

    Time: 2 ms (99.97%) | Memory: 43.7 MB (50.09%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (cnt > 0) sb.append(c);
+                cnt++;
+            } else {
+                cnt--;
+                if (cnt > 0) sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
