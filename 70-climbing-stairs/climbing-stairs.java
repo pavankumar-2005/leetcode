0549d324b29b373bb9814e5247e61b2312586436
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-07-27 18:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (55.07%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,7 +2,7 @@ class Solution {
     public static int waysMem(int n, int[] dp){
         if(n == 0) return 1;
         if(n < 0) return 0;
-        if(dp[n] != -1){
+        if(dp[n] != 0){
             return dp[n];
         }
         dp[n] = waysMem(n - 1, dp) + waysMem(n - 2, dp);
@@ -10,7 +10,6 @@ class Solution {
     }
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
         return waysMem(n, dp);
     }
 }
