c3486b169fa9bb1831ce1be22ec4ae0182f9492c
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-08-02 19:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.59%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,16 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int alice  = 0;
-        int bob = 0;
-        Arrays.sort(piles);
-        for(int i=piles.length-1; i>=0; i--){
-            if(i % 2 != 0){
-                alice += piles[i];
-            }
-            else{
-                bob += piles[i];
-            }
-        }
-        return alice > bob;
+        return true;
     }
 }
