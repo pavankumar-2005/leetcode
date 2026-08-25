7e2f37fbe238c0025eb4a56b3103dbe55e39aef0
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-08-25 12:25:34 +0530

    Time: 5 ms (42.75%) | Memory: 46.1 MB (33.85%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,34 +1,15 @@
 class Solution {
-    public String rev(String r){
-        StringBuilder str = new StringBuilder(r);
-        return str.reverse().toString();
-    }
     public String removeOccurrences(String s, String part) {
-        int n = s.length();
+        StringBuilder sb = new StringBuilder();
         int m = part.length();
-        Stack<Character> stk = new Stack<>();
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            stk.push(ch);
-            if(!stk.isEmpty() && stk.size() >= m && stk.peek() == part.charAt(m-1)){
-                String k = "";
-                for(int j=0; j<m; j++){
-                    if(!stk.isEmpty()){
-                        k += stk.pop();
-                    }
-                }
-                String r = rev(k);
-                if(!r.equals(part)){
-                    for(int l=0; l<r.length(); l++){
-                        stk.push(r.charAt(l));
-                    }
+        for(int i=0; i<s.length(); i++){
+            sb.append(s.charAt(i));
+            if(sb.length() >= m){
+                if(sb.substring(sb.length() - m).equals(part)){
+                    sb.delete(sb.length() - m, sb.length());
                 }
             }
         }
-        String res = "";
-        while(!stk.isEmpty()){
-            res += stk.pop();
-        }
-        return rev(res);
+        return sb.toString();
     }
 }
