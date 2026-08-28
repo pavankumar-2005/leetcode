583f4804357c27d30333e0113bff89207b308764
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-08-28 12:06:53 +0530

    Time: 10 ms (17.58%) | Memory: 47 MB (10.42%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,22 +1,22 @@
 class Solution {
     public String reverseWords(String s) {
-        int n = s.length();
-        StringBuilder str = new StringBuilder();
         String res = "";
-        for(int i=n-1; i>=0; i--){
-            if(s.charAt(i) != ' '){
-                str.append(s.charAt(i));
+        StringBuilder sb = new StringBuilder();
+        for(int i=s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+            if(ch != ' '){
+                sb.append(ch);
             }
             else{
-                if(str.length() > 0){
-                    res += str.reverse().toString();
-                    res += " ";
-                    str.setLength(0);
+                if(!sb.isEmpty()){
+                res += sb.reverse().toString();
+                res += " ";
+                sb.setLength(0);
                 }
             }
         }
-        if(str.length() > 0){
-            res += str.reverse().toString();
+        if(!sb.isEmpty()){
+            res += sb.reverse().toString();
         }
         return res.trim();
     }
