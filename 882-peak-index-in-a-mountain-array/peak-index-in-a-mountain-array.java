06656256e27169be55ee04accd073281e668cbaf
pavankumar-2005 <bayyapavankumar97@gmail.com>
2026-09-04 20:51:05 +0530

    Time: 3 ms (2.04%) | Memory: 80.5 MB (8.28%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,7 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        for(int i=1; i<arr.length; i++){
-            if(arr[i] < arr[i-1]) return i-1;
+        int l = 0, r = arr.length - 1;
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
+                l++;
+            }
+            else{
+                r--;
+            }
         }
         return 0;
     }
